23547430b78241a7c5515dd37a4c83fd46077ce2
salif664 <salibeteye@gmail.com>
2025-11-28 01:46:19 +0100

    update html css js pages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 "// JavaScript code" 
+const button = document.getElementById("btn");
+const message = document.getElementById("message");
+const title = document.getElementById("title");
+
+button.addEventListener("click", function () {
+
+      message.innerText = "JavaScript is now working!";
+    title.style.color = "blue";
+    button.innerText = "Clicked!";
